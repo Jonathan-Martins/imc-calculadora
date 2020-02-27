87378b28f5cf9945a42b94e138647f2ae707d5e9
Jonathan Martins <jonathan.santosmartins@gmail.com>
2020-02-27 18:21:57 -0300

    função para setar o resultado.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 function main() {
     const formElement = document.querySelector('form')
+    const outputElement = document.querySelector('output')
 
     function calcularIMC(peso, altura) {
         let imc = peso / (altura * altura)
@@ -36,6 +37,14 @@ function main() {
             return categorias[0]
         }
     }
+
+    function setResult(msg) {
+        outputElement.innerHTML = ''
+        const p = document.createElement('p')
+        p.innerHTML = msg
+        p.classList.add('resp')
+        outputElement.appendChild(p)
+    }
     
     formElement.addEventListener('submit', (e) => {
         e.preventDefault()
@@ -45,64 +54,10 @@ function main() {
         const altura = Number(inputAltura.value)
         const IMC = calcularIMC(peso, altura)
         const categoria = getCategoria(IMC)
-        console.log(`${IMC}: ${categoria}`)
+        const msg = `Seu IMC é ${IMC}, portanto: ${categoria}`
+        setResult(msg)
     })
 }
 
 main()
 
-/*
-const inputPeso = document.querySelector(".container section input.peso")
-const inputAltura = document.querySelector(".container section input.altura")
-const btnCalcular = document.querySelector(".container section button")
-const outputResp = document.querySelector(".container section.resultado output")
-
-function calcularIMC(peso, altura) {
-    try {
-        if (!peso && !altura) {
-            return alert("Por favor, preencha todos os campos")
-        }
-        
-        if (!peso) {
-            return alert("Por favor, preencha o campo peso")
-        }
-
-        if (!altura) {
-            return alert("Por favor, preencha o campo altura")
-        }        
-
-        let imc =  peso / (altura * altura)
-        return imc
-    } catch (err) {
-        alert("Por favor, informe valores válidos")
-    }
-    
-}
-
-btnCalcular.addEventListener("click", () => {
-    let peso = Number(inputPeso.value);
-    let altura = Number(inputAltura.value);
-    let imc = calcularIMC(peso, altura)
-    
-    if (!imc) {
-        return 
-    }
-
-    if (imc <= 17) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Muito baixo do peso.`
-    } else if (imc > 17 && imc < 18.49) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Abaixo do peso.`
-    } else if (imc >= 18.5 && imc < 24.99) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Peso normal.`
-    } else if (imc >= 25 && imc < 29.99) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Acima do peso`
-    } else if (imc >= 30 && imc < 34.99) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Obesidade I`
-    } else if (imc >= 35 && imc < 39.99) {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. Obesidade II(severa)`
-    } else {
-        outputResp.innerHTML = ` ${imc.toFixed(2)}. `
-    }
-    
-})
-*/
